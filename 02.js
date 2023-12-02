c959094f0cb065318a33dc42e0b4114847b2145b
Tobias <tobias.jansson@gmail.com>
2023-12-02 09:40:22 +0100

    Dag 2 del 2

--- 02.js
+++ 02.js
@@ -9,22 +9,37 @@ const bagg = 13
 const bagb = 14
 
 let gamestot = 0
+let powersum = 0
 lines.forEach(line => {
     const gamem = line.match(/Game (\d+).*/)
     const game = parseInt(gamem[1])
     const sets = line.split(';')
     let gamepossible = true
+    let bestset = [0, 0, 0]
     sets.forEach(set => {
         const cols = getColors(set)
-        if(cols[0]> bagr || cols[1] > bagg || cols[2] > bagb) {
-            gamepossible = false 
-       }
+        if (cols[0] > bagr || cols[1] > bagg || cols[2] > bagb) {
+            gamepossible = false
+        }
+        // del 2
+        if (cols[0] > bestset[0]) {
+            bestset[0] = cols[0]
+        }
+        if (cols[1] > bestset[1]) {
+            bestset[1] = cols[1]
+        }
+        if (cols[2] > bestset[2]) {
+            bestset[2] = cols[2]
+        }
     })
-    if(gamepossible) {
+    powersum += bestset[0] * bestset[1] * bestset[2]
+
+    if (gamepossible) {
         gamestot += game
     }
 })
 console.log('Del 1: ' + gamestot)
+console.log('Del 2: ' + powersum)
 
 function getColors(data) {
     const redm = data.match(/.* (\d+) red.*/)
