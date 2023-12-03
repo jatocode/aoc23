0523665d7fd522e5821461e9445d8c356685a181
Tobias <tobias.jansson@gmail.com>
2023-12-03 23:16:27 +0100

    Dag 3. Fy satan

--- 03.js
+++ 03.js
@@ -5,55 +5,51 @@ const data = fs.readFileSync(args[0], 'utf8')
 const lines = data.split('\n')
 
 let schema = new Map()
-let symbols = new Map()
 let numbers = []
-let maxy = lines.length
-let maxx = 0
 lines.forEach((line, y) => {
     let data = line.split('')
-    maxx = data.length
     for (let x = 0; x < data.length;) {
         const c = data[x]
         const n = parseInt(c)
         if (!isNaN(n)) {
             const part = data.slice(x).join('')
             const num = part.match(/\d+/)[0]
-            numbers[num] = []
+            const id = num + ':' + x + ',' +y
+            numbers[id] = []
             for (let i = x; i < (x + num.length); i++) {
-                //numbers[y + ',' + i] = parseInt(num)
-                numbers[num].push(y + ',' + i)
-                schema.set(y + ',' + i, parseInt(num))
+                numbers[id].push(i + ',' + y)
+                schema.set(i + ',' + y, parseInt(num))
             }
             x += num.length
         } else {
-            schema.set(y + ',' + x, c)
-            if (c != '.') symbols.set(y + ',' + x, c)
+            schema.set(x + ',' + y, c)
             x++
         }
     }
 })
 
 let total = 0
-numbers.forEach((v, number) => {
+for(let number in numbers) {
     let found = false
-    v.forEach((pos) => {
-        let [y, x] = pos.split(',')
-        if(checkAdjacent(x, y) && !found) {
-            console.log(number)
-            total += number
+    const places = numbers[number]
+    places.forEach((pos) => {
+        let [x, y] = pos.split(',')
+        if(!found && checkAdjacent(x, y)) {
+            const sum = parseInt(number.split(':')[0])
+            total += sum
             found = true
         }
     })
-})
+}
 
 function checkAdjacent(x,y) {
     y = parseInt(y)
     x = parseInt(x)
     for (let fy = y - 1; fy <= y + 1; fy++) {
         for (let fx = x - 1; fx <= x + 1; fx++) {
-            const c = schema.get(fy + ',' + fx)
+            const c = schema.get(fx + ',' + fy)
             if(c != undefined && isNaN(c) && c != '.') {
-                console.log(c)
+                console.log('found adjacent for ', x,y, ' at ', fy, fx, c)
                 return true
             }
         }
