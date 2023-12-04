6322a93e841925d5aa440730c78f02bfb9957009
Tobias Jansson <Tobias.Jansson@prevas.se>
2023-12-04 11:22:26 +0100

    Dag 4 del 2

--- 04.js
+++ 04.js
@@ -6,21 +6,37 @@ const lines = data.split('\n')
 
 let cards = []
 lines.forEach(line => {
-    let [card, numbers] = line.split(':')
+    let [_, numbers] = line.split(':')
     let [win, my] = numbers.split('|')
     const winnums = win.split(' ').map(n => parseInt(n)).filter(n => !isNaN(n))
     const mynums = my.split(' ').map(n => parseInt(n)).filter(n => !isNaN(n))
 
-    let winning = 0
+    let points = 0
+    let wins = 0
     for (num of mynums) {
         if (winnums.includes(num)) {
-            winning = winning == 0 ? 1 : winning * 2
+            wins++
+            points = points == 0 ? 1 : points * 2
         }
     }
 
-    cards.push({ winnums, mynums, winning })
+    cards.push({ winnums, mynums, points, wins, instances: 1 })
 })
 
-//console.table(cards)
-const total = cards.reduce((acc, card) => acc + card.winning, 0)
+const total = cards.reduce((acc, card) => acc + card.points, 0)
 console.log('Del 1', total)
+
+for (let i = 0; i < cards.length; i++) {
+    const card = cards[i]
+    for (let ins=0; ins<card.instances; ins++) {
+        for (let j = 0; j < card.wins; j++) {
+            const other = cards[i + j + 1]
+            if (other != undefined) {
+                other.instances++
+            }
+        }
+    }
+}
+
+const totalcards = cards.reduce((acc, card) => acc + card.instances, 0)
+console.log('Del 2', totalcards)
