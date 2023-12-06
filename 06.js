49ce9fe9e6622bbddbca5dfcdcd3cd7e9756f327
Tobias Jansson <Tobias.Jansson@prevas.se>
2023-12-06 08:52:54 +0100

    Dag 6 del 2

--- 06.js
+++ 06.js
@@ -6,21 +6,31 @@ const lines = data.split('\n')
 
 let boats = []
 lines.forEach(line => {
-    if(line.startsWith('Time')) {
+    if (line.startsWith('Time')) {
         const tm = line.split(':')[1].split(' ').map(x => parseInt(x)).filter(x => !isNaN(x))
         boats = tm.map(x => { return { time: x, record: 0, recordbreak: 0 } })
-    } else if(line.startsWith('Distance')) {
+    } else if (line.startsWith('Distance')) {
         const sm = line.split(':')[1].split(' ').map(x => parseInt(x)).filter(x => !isNaN(x))
         boats.forEach((boat, i) => boat.record = sm[i])
     }
 })
 
-boats.forEach(boat => {
-    for(let t=0; t<boat.time; t++) {
-        if(distance(t, boat.time) > boat.record)  boat.recordbreak = boat.recordbreak + 1
-    }
-})
-console.log("Del 1: " + boats.reduce((acc, boat) => acc * boat.recordbreak, 1))
+function calcDistance(boats) {
+    boats.forEach(boat => {
+        for (let t = 0; t < boat.time; t++) {
+            if (distance(t, boat.time) > boat.record) boat.recordbreak = boat.recordbreak + 1
+        }
+    })
+    return boats
+}
+
+const boats1 = calcDistance(boats)
+console.log("Del 1: " + boats1.reduce((acc, boat) => acc * boat.recordbreak, 1))
+
+//const boatspart2 = [{ time: 71530, record: 940200, recordbreak: 0 }]
+const boatspart2 = [{ time: 53897698, record: 313109012141201, recordbreak: 0 }]
+const boats2 = calcDistance(boatspart2)
+console.log("Del 2: " + boats2.reduce((acc, boat) => acc * boat.recordbreak, 1))
 
 function distance(hold, totaltime) {
     const speed = hold * 1
