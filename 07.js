5ae36a8c2bf9a68f65e228cf54031d4ddf20ecb2
Tobias Jansson <Tobias.Jansson@prevas.se>
2023-12-07 12:24:06 +0100

    Dag 7 del 2

--- 07.js
+++ 07.js
@@ -5,16 +5,29 @@ const data = fs.readFileSync(args[0], 'utf8')
 const lines = data.split('\n')
 
 const cards = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+const cards2 = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
+
 let hands = []
+let handswithjoker = []
 lines.forEach(line => {
     const [handt, bidt] = line.split(' ')
-    const hand = handt.split('').map(c => 14 - cards.indexOf(c)) //.sort()
+    const hand = handt.split('').map(c => 12 - cards.indexOf(c))
     const bid = parseInt(bidt)
     hands.push({ handt, hand, value: value(hand), bid })
+
+    // Del 2
+    const hand2 = handt.split('').map(c => 12 - cards2.indexOf(c))
+    const [bestJokerHand, bestJokerValue] = valueWithJoker(hand2)
+    handswithjoker.push({ handt, hand: hand2, jokerhand: bestJokerHand, value: bestJokerValue, bid })
 })
+
 const handsByValue = hands.sort((a, b) => cardSorter(a, b))
-const totalWinnings = handsByValue.reduce((acc, hand, i) => acc + hand.bid * (i+1), 0)
+const totalWinnings = handsByValue.reduce((acc, hand, i) => acc + hand.bid * (i + 1), 0)
 console.log("Del 1: " + totalWinnings)
+
+const handsByValueWithJoker = handswithjoker.sort((a, b) => cardSorter(a, b))
+const totalWinnings2 = handsByValueWithJoker.reduce((acc, hand, i) => acc + hand.bid * (i + 1), 0)
+console.log("Del 2: " + totalWinnings2)
 
 function cardSorter(a, b) {
     if (a.value > b.value) return 1
@@ -27,6 +40,20 @@ function cardSorter(a, b) {
         }
     }
     return 0
+}
+
+function valueWithJoker(hand) {
+    let best = 0
+    let besthand = hand
+    for (let c = 0; c < cards2.length; c++) {
+        const newhand = hand.map(x => x == 0 ? c : x)
+        const val = value(newhand)
+        if (val > best) {
+            best = val
+            besthand = newhand
+        }
+    }
+    return [besthand, best]
 }
 
 function value(hand) {
