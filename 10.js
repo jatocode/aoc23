3f1567952ca2e67682625af459911764aa65e391
Tobias <tobias.jansson@gmail.com>
2023-12-10 18:17:57 +0100

    dag 10, del 1

--- 10.js
+++ 10.js
@@ -10,17 +10,6 @@ lines.forEach((line, i) => {
   grid.push(line.split(''))
 })
 
-/*
-| is a vertical pipe connecting north and south.
-- is a horizontal pipe connecting east and west.
-L is a 90-degree bend connecting north and east.
-J is a 90-degree bend connecting north and west.
-7 is a 90-degree bend connecting south and west.
-F is a 90-degree bend connecting south and east.
-. is ground; there is no pipe in this tile.
-S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has
-*/
-
 const starty = grid.findIndex(row => row.includes('S'))
 const startx = grid[starty].findIndex(col => col === 'S')
 const start = { x: startx, y: starty }
