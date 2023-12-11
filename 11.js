5b3e34f52ef1cc45e27360206bed3cd5767a5d39
Tobias <tobias.jansson@gmail.com>
2023-12-11 22:08:00 +0100

    Snyggade till 11 del 2

--- 11.js
+++ 11.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
-const { get } = require('http')
 const args = process.argv.slice(2)
 
 const data = fs.readFileSync(args[0], 'utf8')
@@ -10,16 +9,14 @@ lines.forEach(line => {
   universe.push(line.split(''))
 })
 
-//printUniverse(universe)
-
-const expanded = expandUniverse(universe)
-const galaxies = countGalaxies(expanded)
+const expands = expandUniverse(universe)
+const galaxies = countGalaxies(universe)
 const galaxypairs = getPairs(galaxies)
-// printUniverse(expanded)
-const total = galaxypairs.reduce((acc, pair) => acc + getDistance(pair[0], pair[1], expanded), 0)
+
+const total = galaxypairs.reduce((acc, pair) => acc + getDistance(pair[0], pair[1], expands), 0)
 console.log('Del 1:', total)
 
-const total2 = galaxypairs.reduce((acc, pair) => acc + getDistance(pair[0], pair[1], expanded, 1000000 - 1), 0)
+const total2 = galaxypairs.reduce((acc, pair) => acc + getDistance(pair[0], pair[1], expands, 1000000 - 1), 0)
 console.log('Del 2:', total2)
 
 function expandUniverse(universe) {
@@ -35,53 +32,33 @@ function expandUniverse(universe) {
     const xexpand = xline.indexOf('#') == -1
     if (xexpand) xexpands.push(x)
   }
-
-  let expanded = []
+  let yexpands = []
   for (let y = 0; y < universe.length; y++) {
     const yexpand = universe[y].find((e) => e === '#') == undefined
-    // om vi får expandera i y-led, skjut in en extra tomrad, annars bara originalraden
-    const expandedx = expandx(universe[y], xexpands)
-    expanded.push(expandedx)
-    if (yexpand) expanded.push(expandedx.join('').replace(/\./g, '*').split(''))
+    if (yexpand) yexpands.push(y)
   }
-  return expanded
+
+  return [xexpands, yexpands]
 }
 
-function expandx(line, xexpands) {
-  let newline = [...line]
-  xexpands.forEach((xp, i) => {
-    newline.splice(xp + i, 0, '*')
-  })
-  return newline
-}
-
-function getDistance(galaxy1, galaxy2, universe, expandsize = 1) {
+function getDistance(galaxy1, galaxy2, expandlist, expandsize = 1) {
   let gx1 = galaxies[galaxy1]
   let gx2 = galaxies[galaxy2]
 
-  const expands = getExpands(galaxy1, galaxy2, universe)
+  let minx = Math.min(gx1.x, gx2.x)
+  let maxx = Math.max(gx1.x, gx2.x)
+  let miny = Math.min(gx1.y, gx2.y)
+  let maxy = Math.max(gx1.y, gx2.y)
+
+  // Hitta hur många expansions mellan galaxerna
+  const xe = expandlist[0].filter(e => e > minx && e < maxx).length
+  const ye = expandlist[1].filter(e => e > miny && e < maxy).length
+  const expands = xe + ye
+
   let dist = Math.abs(gx1.x - gx2.x) + Math.abs(gx1.y - gx2.y)
-  return dist - expands + (expands * expandsize)
-}
 
-function getExpands(galaxy1, galaxy2, universe) {
-  let gx1 = galaxies[galaxy1]
-  let gx2 = galaxies[galaxy2]
-  let xdir = gx1.x < gx2.x ? 1 : -1
-  let ydir = gx1.y < gx2.y ? 1 : -1
-
-  let expands = 0
-  for (let x = gx1.x; x != gx2.x; x += xdir) {
-    if (universe[gx1.y][x] === '*') {
-      expands++
-    }
-  }
-  for (let y = gx1.y; y != gx2.y; y += ydir) {
-    if (universe[y][gx1.x] === '*') {
-      expands++
-    }
-  }
-  return expands
+  // Och lägg till expansionsstorleken
+  return dist + (expands * expandsize-1)
 }
 
 function getPairs(galaxies) {
@@ -96,21 +73,12 @@ function getPairs(galaxies) {
 
 function countGalaxies(universe) {
   let galaxies = []
-  let galaxy = 1
   for (let y = 0; y < universe.length; y++) {
     for (let x = 0; x < universe[y].length; x++) {
       if (universe[y][x] === '#') {
-        universe[y][x] = galaxy++
         galaxies.push({ x, y })
       }
     }
   }
   return galaxies
-}
-
-function printUniverse(universe) {
-  universe.forEach(row => {
-    console.log(row.join(''))
-  })
-  console.log()
 }
