1b99f754eaf3860cfe3b478271a2cc84a9859a67
Tobias <tobias.jansson@gmail.com>
2023-12-12 22:49:58 +0100

    Dag 12, rekursivt

--- 12.js
+++ 12.js
@@ -10,7 +10,8 @@ lines.forEach(line => {
   springs.push({ condition: c, groups: groups.split(',').map(g => parseInt(g)) })
 })
 
-springs.forEach((spring,i) => {
+springs.forEach((spring, i) => {
+  //let spring = springs[1]
   const variants = createVariant(spring.condition, []).sort()
   const matches = matchGroups(variants, spring.groups)
   spring.matches = matches
@@ -18,39 +19,28 @@ springs.forEach((spring,i) => {
 console.table(springs)
 console.log('Del 1: ', springs.reduce((acc, spring) => acc + spring.matches, 0))
 
+function matchGroup(variant, groups, matches = 0) {
+  const numGroups = variant.match(/#+/g)?.length
+  if (numGroups != groups.length) {
+    return matches
+  }
+  const g = groups.shift()
+  if (!g) {
+    return matches
+  }
+  const regex = new RegExp(`(?<!#)#{${g}}(?!#)`)
+  const groupm = variant.match(regex)
+  if (groupm) {
+    index = groupm.index
+    matches = matchGroup(variant.slice(index + g), groups, matches + 1)
+  }
+  return matches
+}
+
 function matchGroups(variants, groups) {
   let matches = 0
-  variants.forEach((variant,i) => {
-    let allMatch = true
-    const numGroups = variant.match(/#+/g)?.length
-    // Rätt antal grupper?
-    if (numGroups != groups.length) {
-      allMatch = false
-    }
-    let index = 0
-    for (let i = 0; i < groups.length; i++ && allMatch == true) {
-      const g = groups[i]
-      // Ville göra sjuka grejor med regexp
-      // https://stackoverflow.com/questions/43174409/regex-that-matches-no-more-than-n-occurrences
-      const re = new RegExp(`(?<!#)#{${g}}(?!#)`)
-      const group = variant.match(re)
-      if (group) {
-        // Grupperna måste vara i ordning
-        if(group.index < index) {
-          allMatch = false
-          break
-        }
-        index = group.index
-        const varr = [...variant]
-        varr.splice(group.index, group[0].length, '.'.repeat(g))
-        variant = varr.join('')
-      } else {
-        // Ingen träff alls
-        allMatch = false
-        break
-      }
-    }
-    if (allMatch) {
+  variants.forEach(variant => {
+    if (matchGroup(variant, [...groups]) == groups.length) {
       matches++
     }
   })
