a1e030841f06241057be54d29d5b0ff24b1ea738
Tobias <tobias.jansson@gmail.com>
2023-12-14 07:29:04 +0100

    dag 14 testet funkar

---
+++ 14.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+const args = process.argv.slice(2)
+
+const rawdata = fs.readFileSync(args[0], 'utf8')
+const lines = rawdata.split('\n')
+
+let rocks = []
+lines.forEach(line => {
+  rocks.push(line.split(''))
+})
+console.table(rocks)
+shiftNorth(rocks)
+console.table(rocks)
+console.log('Del 1: ', calcLoad(rocks))
+
+function calcLoad(rocks) {
+  let load = 0
+  for (let y = 0; y < rocks.length; y++) {
+    for (let x = 0; x < rocks[y].length; x++) {
+      if (rocks[y][x] == 'O') {
+        load += (rocks.length - y) 
+      }
+    }
+  }
+  return load
+}
+
+function shiftNorth(rocks) {
+  for (let y = 0; y < rocks.length; y++) {
+    for (let x = 0; x < rocks[y].length; x++) {
+        rollRock(x,y)
+    }
+  }
+}
+
+function rollRock(x,y) {
+  if(rocks[y-1] == undefined) {
+    return [x,y]
+  }
+  if (rocks[y][x] == 'O') {
+    const over = rocks[y-1][x]
+    switch (over) {
+      case 'O':
+        return [x,y]
+      case '.':
+        rocks[y-1][x] = 'O'
+        rocks[y][x] = '.'
+        return rollRock(x,y-1)
+      case '#':
+        return [x,y]
+    } 
+  }
+}
