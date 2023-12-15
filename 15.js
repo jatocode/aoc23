a6b1378e1f23c49d975beb024dda4352bd21c4fe
Tobias Jansson <tobias.jansson@prevas.se>
2023-12-15 11:26:17 +0100

    15 dag 2

--- 15.js
+++ 15.js
@@ -10,13 +10,52 @@ lines.forEach(line => {
 })
 
 let hashes = []
+let boxes = []
 initseq.forEach(seq => {
-  const h = hash(seq)
-  //console.log(seq, h)
-  hashes.push(h)
+  const hn = hash(seq)
+  hashes.push(hn)
+  let op = ''
+  let label = ''
+  let focal = 0
+  if (seq.includes('=')) {
+    op = '='
+    const [l, f] = seq.split('=')
+    label = l
+    focal = parseInt(f)
+  }
+  if (seq.includes('-')) {
+    op = '-'
+    label = seq.split('-')[0]
+  }
+  const box = hash(label)
+  if (boxes[box] === undefined) boxes[box] = []
+  switch (op) {
+    case '=': {
+      const found = boxes[box].find(slot => slot.label === label)
+      if (found !== undefined) {
+        found.focal = focal
+      } else {
+        boxes[box].push({ label, focal })
+      }
+      break
+    }
+    case '-': {
+      found = boxes[box].find(slot => slot.label === label)
+      if (found !== undefined) {
+        boxes[box].splice(boxes[box].indexOf(found), 1)
+      }
+      break
+    }
+  }
 });
 
-console.log('Del 1: ', hashes.reduce((a, b) => a + b, 0)) 
+console.log('Del 1: ', hashes.reduce((a, slot) => a + slot, 0))
+console.log('Del 2: ', boxtotals(boxes))
+
+function boxtotals(boxes) {
+  return boxes.reduce((tot, slot, bi) => tot + (slot.reduce((tf,slot,si) => tf + (bi+1) * (slot.focal * (si+1)), 0)), 0)
+}
+
 function hash(seq) {
   let h = 0
   for (let i = 0; i < seq.length; i++) {
