a9a1e26634a089347ec076930fab40fc92bc4add
Tobias Jansson <tobias.jansson@prevas.se>
2023-12-22 09:46:42 +0100

    20 försök 2. Nästan i mål

--- 20.js
+++ 20.js
@@ -5,7 +5,12 @@ const rawdata = fs.readFileSync(args[0], 'utf8')
 const lines = rawdata.split('\n')
 
 let data = []
-let broadcastmodule = { module: 'broadcast', dest: [], mem: new Map() }
+const broadcastmodule = { name: 'broadcast', dest: [], mem: new Map() }
+data.push(broadcastmodule)
+let pulses = []
+let lowpulse = 0
+let highpulse = 0
+
 for (let i = 0; i < lines.length; i++) {
   const line = lines[i]
   if (line.startsWith('broadcast')) {
@@ -14,50 +19,76 @@ for (let i = 0; i < lines.length; i++) {
   const m = line.match(/([\%\&])(\w+) -> (.*)/)
   if (m) {
     const dest = m[3].split(',').map(d => d.trim())
-    data.push({ module: m[2], op: m[1], dest, val: 0, mem: new Map() })
+    data.push({ name: m[2], op: m[1], dest, val: 0, mem: new Map() })
   }
 }
-pushButton()
+
+for (let i = 0; i < 1000; i++) {
+  pushButton()
+}
+console.log('lowpulse', lowpulse)
+console.log('highpulse', highpulse)
+console.log('Del 1:', (lowpulse ) * (highpulse ))
 
 function pushButton() {
-  sendPulse(0, broadcastmodule)
-  data.forEach(m => {
-    sendPulse(m.val, m)
-  })
-  console.table(data)
-}
-
-function sendPulse(pulse, module) {
-  const dest = module.dest
-  console.log(module.module, ' ' + pulse == 0 ? '-low-' : '-high-' + ' -> ', dest)
-  dest.forEach(d => {
-    const destm = data.find(m => m.module === d)
-    if (destm) {
-      const val = module.val
-      switch (destm.op) {
-        case '%':
-          destm.val = flipflop(pulse, val)
-          updateMem(destm, module, val)
-          sendPulse(destm.val, destm)
-          break
-        case '&':
-          updateMem(destm, module, val)
-          const output = conjunction(val, module)
-          break
-      }
+  pulses.push({ dest: 'broadcast', pulse: 0 })
+  lowpulse++
+  while (pulses.length > 0) {
+    const pulse = pulses.shift()
+    const module = data.find(m => m.name === pulse.dest)
+    if (module) {
+      generatePulses(pulse.pulse, module)
     }
-  })
+  }
 }
 
-function updateMem(destmodule, module, val) {
-  if(module.module === 'broadcast') return
-  console.log('updatemem', destmodule.module, module.module, val)
-  destmodule.mem.set(module.module, val)
+function generatePulses(pulse, module) {
+  let output = undefined
+  let sendpulse = false
+  if (module.name === 'broadcast') {
+    output = 0
+    sendpulse = true
+  }
+
+  switch (module.op) {
+    case '%':
+      if (pulse == 0) {
+        output = flipflop(pulse, module.val)
+        module.val = output
+        sendpulse = true
+      }
+      break
+    case '&':
+      output = conjunction(module)
+      module.val = output
+      sendpulse = true
+      break
+  }
+
+  if (sendpulse) {
+    module.dest.forEach(d => {
+      //console.log(module.name, ' ' + output == 0 ? '-low-' : '-high-' + ' -> ', d)
+      updateMem(d, module, output)
+      pulses.push({ dest: d, pulse: output })
+      lowpulse += output === 0 ? 1 : 0
+      highpulse += output === 1 ? 1 : 0
+    })
+  }
 }
 
-function conjunction(val, m) {
-  console.log('conjunction', m.module)
+function updateMem(destname, module, val) {
+  if (destname === 'broadcast') return
+  const destmodule = data.find(m => m.name === destname)
+  if (!destmodule) return
+  destmodule.mem.set(module.name, val)
+}
 
+function conjunction(module) {
+  if (module.mem.size === 0) {
+    return 1
+  }
+  const allone = Array.from(module.mem.values()).every(v => v === 1)
+  return allone ? 0 : 1
 }
 
 function flipflop(pulse, val) {
